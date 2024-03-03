1456eeb74c224cc06a0c4f4c1e99d5f41dc65f7f
Samir Ranjan Parida <paridasamirranjan@gmail.com>
2024-03-03 19:10:44 +0530

    New commit

---
+++ JS/JS_Chapter_59/index.js
@@ -0,0 +1,15 @@
+/*
+Create a faulty calculator using JavaScript.
+
+This faulty calculaor does the following:
+1. It takes two no's as input from user.
+2.It performs wrong functions as follows:-
+  + --> -
+  * --> +
+  - --> /
+  / --> **
+
+  ** It performs wrong calculation 10% of the time.
+
+  */
+
